77e1428df15f07349ee0a1b406c0f442500c3057
OlegMilkin <olegek.milkin@gmail.com>
2021-10-15 18:17:19 +0300

    slicing

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,1 @@
-let backToTop = $('.js_back-top');
-
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 0) {
-    backToTop.addClass('back-to-top--active');
-  } else {
-    backToTop.removeClass('back-to-top--active');
-  }
-});
-
-backToTop.on('click', function(e){
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-  e.preventDefault();
-});
+//Your scripts will be here
